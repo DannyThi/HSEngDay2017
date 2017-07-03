fb13f87944cdd9f378c4e6a12176553a47cd5088
Thi Danny <Danny@Danny-This-MacBook-Pro.local>
2017-07-03 14:06:03 +0900

    Added audio programming.

---
+++ Assets/_Scripts/_GameController/AudioController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+
+	public float delayStart = 10;
+
+	private float time = 0;
+
+	private AudioSource audioSource;
+
+	void Awake() {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void Start() {
+		time = Time.time;
+	}
+
+	void Update() {
+		time = Time.time - time;
+		if (time >= delayStart) {
+			audioSource.Play ();
+		}
+	}
+}
