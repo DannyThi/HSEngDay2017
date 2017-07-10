9e939d6710c1165aa8859b0b0a3ef187993698e2
Thi Danny <Danny@Danny-This-MacBook-Pro.local>
2017-07-10 14:12:02 +0900

    Adding glitch.

--- Assets/_Scripts/_LevelController/FinalLevel.cs
+++ Assets/_Scripts/_LevelController/FinalLevel.cs
@@ -4,27 +4,51 @@ using UnityEngine;
 
 public class FinalLevel : MonoBehaviour {
 
-	public float delayStart = 0;
+	public float delayStartForTransition = 0;
+
 	public MovieTexture movieTexture;
 	public bool isEscapable = false;
 
 
+	private GameObject movieQuad;
 	private AudioSource audioSource;
 
+
+	public delegate void TransitionNotification();
+	public static event TransitionNotification OnTransitionTrigger;
+
 	void Awake() {
+		movieQuad = GameObject.FindGameObjectWithTag ("MovieQuad");
 		audioSource = GetComponent<AudioSource> ();
+		movieQuad.GetComponent<Renderer>().material.mainTexture = movieTexture;
 	}
 
 	void Start() {
-		GameController.InitialGameEndNotification ();
-
 		Debug.Log(name + " has been loaded.");
+		GameController.InitialGameEndNotification ();
 		audioSource.clip = movieTexture.audioClip;
+
+		// change aspect fit
+		//movieQuad.GetComponent<Renderer>().material.SetTextureScale
+
 		StartCoroutine (PlayMovie ());
 	}
 
 	private IEnumerator PlayMovie() {
-		yield return new WaitForSeconds (delayStart);
+		if (OnTransitionTrigger != null) {
+			OnTransitionTrigger ();
+		} else {
+			Debug.Log(name + ": no OnTransitionTrigger.");
+		}
+
+		yield return new WaitForSeconds (delayStartForTransition);
+	
+		// glitch camera notification
+
+
+		// enable movieQuad renderer
+		movieQuad.GetComponent<MeshRenderer> ().enabled = true;
+
 		if (movieTexture != null) {
 			movieTexture.Play ();
 			audioSource.Play ();
@@ -36,12 +60,12 @@ public class FinalLevel : MonoBehaviour {
 		}
 	}
 
-
-	void OnGUI() {
-		if (movieTexture != null && movieTexture.isPlaying) {
-			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), movieTexture, ScaleMode.ScaleToFit);
-		}
-	}
+//
+//	void OnGUI() {
+//		if (movieTexture != null && movieTexture.isPlaying) {
+//			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), movieTexture, ScaleMode.ScaleToFit);
+//		}
+//	}
 
 	void Update() {
 		if (isEscapable == true) {
@@ -52,4 +76,6 @@ public class FinalLevel : MonoBehaviour {
 			}
 		}
 	}
+
+
 }
---
+++ Assets/_Scripts/_Player/GlitchTransition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlitchTransition : MonoBehaviour {
+
+	public float glitchValue = 1;
+	private Kino.AnalogGlitch analogue;
+	private Kino.DigitalGlitch digital;
+
+	void Awake() {
+		analogue = GetComponent<Kino.AnalogGlitch> ();
+		digital = GetComponent<Kino.DigitalGlitch> ();
+	}
+
+	void Start() {
+		glitchValue = Mathf.Clamp (glitchValue, 0.1f, glitchValue);
+	}
+
+	IEnumerator Glitch() {
+		// start glitch in a loop until value is = to value
+		// wait
+		// unglitch back to 0.1
+		yield return null;
+
+	}
+
+	private void IntiateAnalogueGlitch(float value) {
+		analogue.scanLineJitter = value;
+		analogue.verticalJump = value;
+		analogue.horizontalShake = value;
+		analogue.colorDrift = value;
+	}
+
+	private void InitiateDigitalGlitch(float value) {
+		digital.intensity = value;
+	}
+
+	private void HandleTransitionNotification() {
+		//StartCoroutine (Glitch ());
+	}
+
+	void OnEnable() {
+		FinalLevel.OnTransitionTrigger += HandleTransitionNotification;
+	}
+
+	void OnDisable() {
+		FinalLevel.OnTransitionTrigger -= HandleTransitionNotification;
+	}
+}
