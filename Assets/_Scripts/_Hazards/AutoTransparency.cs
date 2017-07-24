92311fb26c78088fcc2a0f1c713846021dc76411
Thi Danny <Danny@Danny-This-MacBook-Pro.local>
2017-07-24 14:04:02 +0900

    Adding auto-transparency.

---
+++ Assets/_Scripts/_Hazards/AutoTransparency.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoTransparency : MonoBehaviour {
+
+	public const float minAlpha = 0.1f;
+	public const float maxAlpha = 1.0f;
+	public float fadeLength = 0.2f;
+
+	private float targetAlpha;
+	private float startAlpha;
+
+	private Renderer materialRenderer;
+	private Color color;
+
+	void Awake() {
+		color = GetComponentInChildren<Renderer> ().material.color;
+	}
+
+	private IEnumerator Fade() {
+		Debug.Log ("Starting Fade");
+		while (color.a >= minAlpha) {
+			color.a = Mathf.Lerp (startAlpha, targetAlpha, Time.deltaTime * fadeLength);
+			yield return new WaitForFixedUpdate();
+		}
+		color.a = targetAlpha;
+	}
+
+	public void FadeToMinAlpha() {
+		startAlpha = color.a;
+		targetAlpha = minAlpha;
+		StartCoroutine (Fade ());
+	}
+
+	public void FadeToMaxAlpha() {
+		Debug.Log ("Received fade request.");
+		startAlpha = color.a;
+		targetAlpha = maxAlpha;
+		StartCoroutine (Fade ());
+	}
+
+}
---
+++ Assets/_Scripts/_Hazards/FadeObscuringObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeObscuringObject : MonoBehaviour {
+
+	public float distanceToCamera = 100.0f;
+	public float updateInterval = 2.0f;
+
+	private Camera cam;
+
+	// 15 = 3dArrows Layer
+	// 16 = WordContainer Layer
+	//private int layerMask = (1 << 15) | (1 << 16);
+	private float nextUpdate = 0;
+
+	void Awake() {
+		cam = GameObject.FindGameObjectWithTag ("FollowCam").GetComponent<Camera> ();
+	}
+
+	void Update() {
+		Raycasting ();
+	}
+
+
+	private void Raycasting() {
+		// update every two seconds
+		if (Time.time > nextUpdate) 
+		{
+			Debug.Log ("Raycasting.");
+			nextUpdate = Time.time + updateInterval;
+
+			RaycastHit[] raycastHits = Physics.RaycastAll (
+				transform.position, 
+				cam.transform.position,
+				distanceToCamera
+			);
+
+			foreach (RaycastHit hit in raycastHits) {
+				Debug.Log ("Going through hits.");
+				AutoTransparency transparency = hit.transform.gameObject.GetComponent<AutoTransparency> ();
+				if (transparency != null) {
+					Debug.Log ("Calling transparency fade method.");
+					transparency.FadeToMinAlpha ();
+				}
+			}
+		}
+	}
+}
